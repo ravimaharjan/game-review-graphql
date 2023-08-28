958fea1afbf3404ac000a6c03c37cc39a7136bb3
ravi <mail.meravi00@gmail.com>
2023-08-28 17:37:04 +0545

    -added filter in the Game

--- src/resolvers/Game.js
+++ src/resolvers/Game.js
@@ -1,6 +1,19 @@
 const game = {
-    reviews({ id }, args, { reviews }) {
-        return reviews.filter(review => review.game_id === id)
+    reviews({ id }, { filter }, { reviews, authors }) {
+        const hasFilter = filter?.authorVerified !== undefined ? true : false;
+        const filteredReviews = reviews.filter(review => review.game_id === id);
+
+        if (!hasFilter) return filteredReviews;
+
+        const verifiedAuthorsSet = new Set();
+
+        authors.forEach(author => {
+            if (author.verified === filter.authorVerified) verifiedAuthorsSet.add(author.id);
+        })
+
+        return filteredReviews.filter(review => {
+            return verifiedAuthorsSet.has(review.author_id);
+        })
     }
 }
 
